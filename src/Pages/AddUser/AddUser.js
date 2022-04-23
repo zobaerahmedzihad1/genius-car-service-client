e41316fbbcebf4d5ea407692a674b8223b1c6eed
Zobaer Ahmed Zihad <zobaerahmedzihad02@gmail.com>
2022-04-23 23:28:16 +0600

    Insert Service data to the mongodb cloud database

--- src/Pages/AddUser/AddUser.js
+++ src/Pages/AddUser/AddUser.js
@@ -3,14 +3,44 @@ import { useForm } from "react-hook-form";
 
 const AddUser = () => {
   const { register, handleSubmit } = useForm();
-  const onSubmit = (data) => console.log(data);
+  const onSubmit = (data) => {
+    const url = "http://localhost:5000/service";
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((result) => console.log(result));
+  };
   return (
-    <div>
-      <h2>Add user</h2>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <input {...register("firstName", { required: true, maxLength: 20 })} />
-        <input {...register("lastName", { pattern: /^[A-Za-z]+$/i })} />
-        <input type="number" {...register("age", { min: 18, max: 99 })} />
+    <div className="w-50 mx-auto">
+      <h2>Add new user</h2>
+      <form className="d-flex flex-column" onSubmit={handleSubmit(onSubmit)}>
+        <input
+          placeholder="Name"
+          className="mb-2"
+          {...register("name", { required: true, maxLength: 20 })}
+        />
+        <textarea
+          placeholder="Description"
+          className="mb-2"
+          {...register("description")}
+        />
+        <input
+          placeholder="price"
+          className="mb-2"
+          type="number"
+          {...register("price")}
+        />
+        <input
+          placeholder="Photo url"
+          className="mb-2"
+          type="text"
+          {...register("img")}
+        />
         <input type="submit" />
       </form>
     </div>
