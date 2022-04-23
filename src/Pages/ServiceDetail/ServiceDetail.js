5ff7d044a9444bd93b80e3fe252e605fe70c65f4
Zobaer Ahmed Zihad <zobaerahmedzihad02@gmail.com>
2022-04-23 09:16:57 +0600

    genius car service client

--- src/Pages/ServiceDetail/ServiceDetail.js
+++ src/Pages/ServiceDetail/ServiceDetail.js
@@ -1,18 +1,30 @@
-import React from 'react';
-import { Link, useParams } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
 
 const ServiceDetail = () => {
-    const { serviceId } = useParams();
-    return (
-        <div>
-            <h2>Welcome to detail: {serviceId}</h2>
-            <div className='text-center'>
-                <Link to="/checkout">
-                    <button className='btn btn-primary'>Proceed Checkout</button>
-                </Link>
-            </div>
-        </div>
-    );
+
+const [service , setService] = useState({})
+
+  const { serviceId } = useParams();
+
+  useEffect(() => {
+    const url = `http://localhost:5000/service/${serviceId}`;
+    console.log(url);
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => setService(data));
+  }, [serviceId]);
+
+  return (
+    <div>
+      <h2>Welcome to detail: {service.name}</h2>
+      <div className="text-center">
+        <Link to="/checkout">
+          <button className="btn btn-primary">Proceed Checkout</button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default ServiceDetail;
